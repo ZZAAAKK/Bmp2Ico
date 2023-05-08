95beef1152312e9f8ee95a5c501ca78fcdf27bc4
ZZAAAKK <48885763+ZZAAAKK@users.noreply.github.com>
2023-05-08 21:36:13 +0100

    Add project files.

---
+++ Bmp2Ico/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Bmp2Ico
+{
+    internal class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        extern static bool DestroyIcon(IntPtr handle);
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string filepath = args[0];
+
+                Bitmap bmp = new Bitmap(filepath);
+                IntPtr hicon = bmp.GetHicon();
+
+                Icon icon = Icon.FromHandle(hicon);
+
+                FileStream fileStream = new FileStream(Path.ChangeExtension(filepath, ".ico"), FileMode.OpenOrCreate);
+                icon.Save(fileStream);
+
+                fileStream.Close();
+                DestroyIcon(hicon);
+
+                fileStream.Close();
+                fileStream.Dispose();
+            }
+        }
+    }
+}
